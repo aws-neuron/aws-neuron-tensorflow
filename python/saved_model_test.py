2703fb099215db10286ede2f4284720adf7e1750
Haichen "HC" Li <lhaiche@amazon.com>
2019-11-11 18:28:58 -0800

    [saved_model] change default tags/signature_def_key reading mechanism to hardcoded default first and then reading from SavedModel
    Signed-off-by: Haichen "HC" Li <lhaiche@amazon.com>
    
    cr https://code.amazon.com/reviews/CR-15507350

--- python/saved_model_test.py
+++ python/saved_model_test.py
@@ -138,6 +138,148 @@ def test_convert_to_inference_model_with_feed_dict():
             np.testing.assert_allclose(result_neuron[name], result_ref[name], rtol=1e-2, atol=1e-3)
 
 
+def test_convert_to_inference_model_multi_tags_multi_sigdefs():
+    np.random.seed(_RANDOM_SEED)
+    model_dir = './original_saved_model_multi_tags_multi_sigdefs'
+    new_model_dir0 = './neuron_saved_model_multi_tags_multi_sigdefs0'
+    new_model_dir1 = './neuron_saved_model_multi_tags_multi_sigdefs1'
+    new_model_dir2 = './neuron_saved_model_multi_tags_multi_sigdefs2'
+    tags0 = [tf.saved_model.tag_constants.SERVING, tf.saved_model.tag_constants.GPU]
+    tags1 = [tf.saved_model.tag_constants.TPU, tf.saved_model.tag_constants.GPU]
+    with tf.Session(graph=tf.Graph()) as sess:
+        input0 = tf.placeholder(tf.float16, [None, 2, 2, 3], name='input0')
+        input1 = tf.placeholder(tf.float16, [None, 2, 2, 3], name='input1')
+        input2 = tf.placeholder(tf.float16, [None, 2, 2, 3], name='input2')
+        conv2d0 = tf.nn.conv2d(input0, np.random.uniform(-1, 1, size=[1, 1, 3, 3]).astype(np.float16),
+                               strides=[1, 1, 1, 1], padding='VALID', name='conv2d0')
+        conv2d1 = tf.nn.conv2d(input1, np.random.uniform(-1, 1, size=[1, 1, 3, 3]).astype(np.float16),
+                               strides=[1, 1, 1, 1], padding='VALID', name='conv2d1')
+        add0 = tf.add(conv2d0, conv2d1, name='add0')
+        relu0 = tf.nn.relu(add0, name='relu0')
+        output0 = tf.identity(relu0, name='output0')
+        sigmoid0 = tf.sigmoid(add0, name='sigmoid0')
+        conv2d2 = tf.nn.conv2d(sigmoid0, np.random.uniform(-1, 1, size=[1, 1, 3, 3]).astype(np.float16),
+                               strides=[1, 1, 1, 1], padding='VALID', name='conv2d1')
+        relu1 = tf.nn.relu(conv2d2, name='relu1')
+        output1 = tf.identity(relu1, name='output1')
+        add1 = tf.add(input2, relu1, name='add1')
+        relu2 = tf.nn.relu(add1, name='relu2')
+        output2 = tf.identity(relu2, name='output2')
+        shutil.rmtree(model_dir, ignore_errors=True)
+
+        inputs_sigdef0 = {
+            'input0': tf.saved_model.utils.build_tensor_info(input0),
+            'input1': tf.saved_model.utils.build_tensor_info(input1),
+        }
+        outputs_sigdef0 = {
+            'output0': tf.saved_model.utils.build_tensor_info(output0),
+            'output1': tf.saved_model.utils.build_tensor_info(output1),
+        }
+        sigdef0 = tf.saved_model.signature_def_utils.build_signature_def(
+            inputs=inputs_sigdef0,
+            outputs=outputs_sigdef0,
+            method_name='method0',
+        )
+
+        inputs_sigdef1 = {
+            'input2': tf.saved_model.utils.build_tensor_info(input0),  # intentionally flipped
+            'input1': tf.saved_model.utils.build_tensor_info(input1),
+            'input0': tf.saved_model.utils.build_tensor_info(input2),  # intentionally flipped
+        }
+        outputs_sigdef1 = {
+            'output0': tf.saved_model.utils.build_tensor_info(output2),
+            'output2': tf.saved_model.utils.build_tensor_info(output0),
+        }
+        sigdef1 = tf.saved_model.signature_def_utils.build_signature_def(
+            inputs=inputs_sigdef1,
+            outputs=outputs_sigdef1,
+            method_name='method1',
+        )
+
+        inputs_sigdef2 = {
+            'input0': tf.saved_model.utils.build_tensor_info(input0),
+            'input2': tf.saved_model.utils.build_tensor_info(input2),
+            'input1': tf.saved_model.utils.build_tensor_info(input1),
+        }
+        outputs_sigdef2 = {
+            'output2': tf.saved_model.utils.build_tensor_info(output2),
+            'output1': tf.saved_model.utils.build_tensor_info(output1),
+        }
+        sigdef2 = tf.saved_model.signature_def_utils.build_signature_def(
+            inputs=inputs_sigdef2,
+            outputs=outputs_sigdef2,
+            method_name='method2',
+        )
+
+        inputs_sigdef3 = {
+            'input3': tf.saved_model.utils.build_tensor_info(input2),
+            'input0': tf.saved_model.utils.build_tensor_info(input0),
+            'input1': tf.saved_model.utils.build_tensor_info(input1),
+        }
+        outputs_sigdef3 = {
+            'output3': tf.saved_model.utils.build_tensor_info(output2),
+            'output1': tf.saved_model.utils.build_tensor_info(output1),
+        }
+        sigdef3 = tf.saved_model.signature_def_utils.build_signature_def(
+            inputs=inputs_sigdef3,
+            outputs=outputs_sigdef3,
+            method_name='method3',
+        )
+
+        signature_def_map0 = {
+            'sigdef0': sigdef0,
+            'sigdef1': sigdef1,
+            'sigdef2': sigdef2,
+        }
+        signature_def_map1 = {
+            'sigdef3': sigdef3,
+            'sigdef2': sigdef2,
+        }
+
+        builder = tf.saved_model.builder.SavedModelBuilder(model_dir)
+        builder.add_meta_graph_and_variables(sess, tags0, signature_def_map=signature_def_map0,
+                                             strip_default_attrs=True)
+        builder.add_meta_graph(tags1, signature_def_map=signature_def_map1, strip_default_attrs=True)
+        builder.save()
+
+    feed_dict = {
+        'input0:0': np.random.uniform(-1, 1, size=[1, 2, 2, 3]).astype(np.float16),
+        'input1:0': np.random.uniform(-1, 1, size=[1, 2, 2, 3]).astype(np.float16),
+        'input2:0': np.random.uniform(-1, 1, size=[1, 2, 2, 3]).astype(np.float16),
+    }
+    model_feed_dict0 = {key: feed_dict[inputs_sigdef0[key].name] for key in inputs_sigdef0.keys()}
+    model_feed_dict1 = {key: feed_dict[inputs_sigdef1[key].name] for key in inputs_sigdef1.keys()}
+    model_feed_dict2 = {key: feed_dict[inputs_sigdef2[key].name] for key in inputs_sigdef2.keys()}
+    shutil.rmtree(new_model_dir0, ignore_errors=True)
+    shutil.rmtree(new_model_dir1, ignore_errors=True)
+    shutil.rmtree(new_model_dir2, ignore_errors=True)
+    tf.neuron.saved_model.compile(model_dir, new_model_dir0, signature_def_key='sigdef0', model_feed_dict=model_feed_dict0)
+    tf.neuron.saved_model.compile(model_dir, new_model_dir1, signature_def_key='sigdef1', model_feed_dict=model_feed_dict1)
+    tf.neuron.saved_model.compile(model_dir, new_model_dir2, signature_def_key='sigdef2', model_feed_dict=model_feed_dict2)
+
+    tags = 'serve,gpu'
+    pred_ref0 = tf.contrib.predictor.from_saved_model(model_dir, tags=tags, signature_def_key='sigdef0')
+    pred_ref1 = tf.contrib.predictor.from_saved_model(model_dir, tags=tags, signature_def_key='sigdef1')
+    pred_ref2 = tf.contrib.predictor.from_saved_model(model_dir, tags=tags, signature_def_key='sigdef2')
+    pred_neuron0 = tf.contrib.predictor.from_saved_model(new_model_dir0, tags=tags, signature_def_key='sigdef0')
+    pred_neuron1 = tf.contrib.predictor.from_saved_model(new_model_dir1, tags=tags, signature_def_key='sigdef1')
+    pred_neuron2 = tf.contrib.predictor.from_saved_model(new_model_dir2, tags=tags, signature_def_key='sigdef2')
+    _assert_compiler_success(pred_neuron0.graph)
+    if 'NEURON_RTD_ADDRESS' in os.environ:
+        result_ref0 = pred_ref0(model_feed_dict0)
+        result_ref1 = pred_ref1(model_feed_dict1)
+        result_ref2 = pred_ref2(model_feed_dict2)
+        result_neuron0 = pred_neuron0(model_feed_dict0)
+        result_neuron1 = pred_neuron1(model_feed_dict1)
+        result_neuron2 = pred_neuron2(model_feed_dict2)
+        for name in result_ref0.keys():
+            np.testing.assert_allclose(result_neuron0[name], result_ref0[name], rtol=1e-2, atol=1e-3)
+        for name in result_ref1.keys():
+            np.testing.assert_allclose(result_neuron1[name], result_ref1[name], rtol=1e-2, atol=1e-3)
+        for name in result_ref2.keys():
+            np.testing.assert_allclose(result_neuron2[name], result_ref2[name], rtol=1e-2, atol=1e-3)
+
+
 def test_convert_to_inference_model_regress_api():
     np.random.seed(_RANDOM_SEED)
     model_dir = './original_saved_model_regress'
@@ -162,7 +304,7 @@ def test_convert_to_inference_model_regress_api():
                 method_name=tf.saved_model.signature_constants.REGRESS_METHOD_NAME
             )
         )
-        signature_def_map = {tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY: serving_signature}
+        signature_def_map = {'bla': serving_signature}
 
         builder = tf.saved_model.builder.SavedModelBuilder(model_dir)
         builder.add_meta_graph_and_variables(sess, tags, signature_def_map=signature_def_map,
@@ -175,8 +317,8 @@ def test_convert_to_inference_model_regress_api():
     shutil.rmtree(new_model_dir, ignore_errors=True)
     tf.neuron.saved_model.compile(model_dir, new_model_dir, model_feed_dict=model_feed_dict)
 
-    pred_ref = tf.contrib.predictor.from_saved_model(model_dir)
-    pred_neuron = tf.contrib.predictor.from_saved_model(new_model_dir)
+    pred_ref = tf.contrib.predictor.from_saved_model(model_dir, signature_def_key='bla')
+    pred_neuron = tf.contrib.predictor.from_saved_model(new_model_dir, signature_def_key='bla')
     _assert_compiler_success(pred_neuron.graph)
     if 'NEURON_RTD_ADDRESS' in os.environ:
         result_ref = pred_ref(model_feed_dict)
