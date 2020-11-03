c8e367fec90d873fc03fea949adc2040c8da984e
Haichen Li <lhaiche@amazon.com>
2020-11-03 23:51:59 +0000

    [open source] change file heads to "Amazon Web Services and its Affiliates"
    cr https://code.amazon.com/reviews/CR-37914350

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-# Copyright 2020 AWS Neuron. All Rights Reserved.
+# Copyright Amazon Web Services and its Affiliates. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
