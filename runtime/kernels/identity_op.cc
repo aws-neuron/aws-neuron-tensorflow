65530a9c70b47f73431587835811b5ff9f34b927
Haichen Li <lhaiche@amazon.com>
2021-04-07 06:12:58 +0000

    [kernels] implement IdentityOp for AWS_NEURON device

---
+++ runtime/kernels/identity_op.cc
@@ -0,0 +1,40 @@
+/* Copyright Amazon Web Services and its Affiliates. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include "registration.h"
+#include "../device.h"
+
+namespace tensorflow {
+namespace neuron {
+
+class IdentityOp : public OpKernel {
+ public:
+  explicit IdentityOp(OpKernelConstruction* context) : OpKernel(context) {}
+
+  void Compute(OpKernelContext* context) override {
+    if (IsRefType(context->input_dtype(0))) {
+      context->forward_ref_input_to_ref_output(0, 0);
+    } else {
+      context->set_output(0, context->input(0));
+    }
+  }
+
+  bool IsExpensive() override { return false; }
+};
+
+NEURON_REGISTER_KERNEL_BUILDER("Identity", DEVICE_NEURON, IdentityOp);
+
+}  // namespace neuron
+}  // namespace tensorflow
