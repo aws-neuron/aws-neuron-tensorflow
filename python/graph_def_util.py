27a4e0f9b281b3e0b9d8cbecb23ca06a10f7237e
Haichen Li <lhaiche@amazon.com>
2021-04-09 17:33:34 +0000

    [graph_def_util] remove unused import

--- python/graph_def_util.py
+++ python/graph_def_util.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-import os
 import math
 import reprlib
 from collections import namedtuple
