03b4d5264d2c35075e59a7ad99f99c1ab7d4cacc
Haichen Li <lhaiche@amazon.com>
2022-06-28 00:46:39 +0000

    [saved_model_v2_test] fix list_operators patch error
    SIM: https://i.amazon.com/kaena-11128
    cr: https://code.amazon.com/reviews/CR-71748105

--- python/saved_model_v2_test.py
+++ python/saved_model_v2_test.py
@@ -120,7 +120,7 @@ class TestCompileV1SavedModel(TestV2Only):
         def fake_list_operators():
             return {'Conv2D', 'Const'}
 
-        with patch('tensorflow.neuron.python._trace.list_operators', fake_list_operators):
+        with patch('tensorflow_neuron.python._trace.list_operators', fake_list_operators):
             result_compile = tfn.saved_model.compile(
                 model_dir, new_model_dir, model_feed_dict=feeds,
             )
@@ -154,9 +154,9 @@ class TestCompileV1SavedModel(TestV2Only):
         }
 
         def fake_list_operators():
-            return {'Conv2D', 'Const', 'Add', 'Relu'}
+            return {'Conv2D', 'Const', 'AddV2', 'Relu'}
 
-        with patch('tensorflow.neuron.python._trace.list_operators', fake_list_operators):
+        with patch('tensorflow_neuron.python._trace.list_operators', fake_list_operators):
             result_compile = tfn.saved_model.compile(
                 model_dir, new_model_dir, model_feed_dict=feeds,
             )
