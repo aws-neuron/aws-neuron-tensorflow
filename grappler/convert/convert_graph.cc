aab916b46b351f011f3ce54176d4e9b68094c949
Haichen Li <lhaiche@amazon.com>
2022-10-12 21:26:33 +0000

    [grappler] simplify convert_graph interface
    SIM: https://i.amazon.com/kaena-12810
    cr: https://code.amazon.com/reviews/CR-77563281

--- grappler/convert/convert_graph.cc
+++ grappler/convert/convert_graph.cc
@@ -212,6 +212,54 @@ std::set<std::string> ExpensiveTypeStrings() {
 
 }  // namespace
 
+struct SubGraphParams {
+  SubGraphParams(tensorflow::Graph& inp_graph,
+                 const std::set<int>& subgraph_node_id_numbers,
+                 const std::vector<string>& output_tensor_names,
+                 const std::vector<std::pair<int, int> >& output_indices,
+                 tensorflow::EdgeSet& incoming_edges,
+                 tensorflow::Node* constructed_neuron_node,
+                 const int neuron_op_ind)
+      : graph(&inp_graph),
+        output_names(&output_tensor_names),
+        subgraph_node_ids(&subgraph_node_id_numbers),
+        output_inds(&output_indices),
+        neuron_node(constructed_neuron_node),
+        neuron_op_index(neuron_op_ind) {
+    for (const tensorflow::Edge* edge : incoming_edges) {
+      subgraph_incoming_edges.push_back(edge);
+    }
+  }
+
+  tensorflow::Graph* graph;
+  const std::vector<string>* output_names;
+  const std::set<int>* subgraph_node_ids;
+  const std::vector<std::pair<int, int> >*
+      output_inds;  // {node_id, output_idx}
+  std::vector<const tensorflow::Edge*> subgraph_incoming_edges;
+  tensorflow::Node* neuron_node;
+  const int neuron_op_index;
+};
+
+struct ConvertGraphParams {
+  ConvertGraphParams(tensorflow::Graph& inp_graph,
+                     const std::vector<string>& output_tensor_names,
+                     const std::set<int>& subgraph_node_id_numbers,
+                     int neuron_op_index)
+      : graph(&inp_graph),
+        output_names(&output_tensor_names),
+        subgraph_node_ids(&subgraph_node_id_numbers),
+        neuron_op_count(neuron_op_index) {}
+
+  tensorflow::Graph* graph;
+  const std::vector<string>* output_names;
+  const std::set<int>* subgraph_node_ids;
+  std::vector<std::pair<int, int> > subgraph_outputs;
+  tensorflow::EdgeSet subgraph_incoming_edges;
+  tensorflow::EdgeSet subgraph_outgoing_edges;
+  int neuron_op_count;
+};
+
 class EdgeValidator {
  public:
   // Return true if the specified edge is eligible to be an input edge of the
